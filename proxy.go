83baa7ccf6292fab3cc9990dc249bc73546cce05
Elias Norberg <elias@aisle.se>
2019-10-27 19:23:03 +0100

    Rewrite proxy code

--- proxy.go
+++ proxy.go
@@ -11,6 +11,7 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
 var outputWriter io.Writer
@@ -36,7 +37,6 @@ Packet server->client:
 00000050  0d 6b 80 00 00 10 00 00  05 83 00 00 00 02        |.k............|
 */
 func isStartTTLS(buf []byte) bool {
-
 	if len(buf) < 0x18 {
 		return false
 	}
@@ -57,17 +57,24 @@ type Proxy struct {
 	clientCh chan []byte
 	serverCh chan []byte
 
+	serverIdentCh chan IdentInfo
+	clientIdentCh chan IdentInfo
+
+	useTTLS bool
+
 	done chan struct{}
 }
 
 // Reader forwards all data from client to clientCh, or from server to serverCh
 func NewProxy(clientConn, serverConn net.Conn) *Proxy {
 	p := &Proxy{
-		clientConn: clientConn,
-		serverConn: serverConn,
-		clientCh:   make(chan []byte),
-		serverCh:   make(chan []byte),
-		done:       make(chan struct{}),
+		clientConn:    clientConn,
+		serverConn:    serverConn,
+		clientCh:      make(chan []byte),
+		serverCh:      make(chan []byte),
+		done:          make(chan struct{}),
+		serverIdentCh: make(chan IdentInfo, 100),
+		clientIdentCh: make(chan IdentInfo, 100),
 	}
 
 	return p
@@ -75,10 +82,21 @@ func NewProxy(clientConn, serverConn net.Conn) *Proxy {
 
 // SetupTTLS wraps an existing connection with a TTLS connection,
 // and returns the TTLS connection
-func (p *Proxy) SetupTTLS(original net.Conn, dir communicationDir) error {
+func (p *Proxy) SetupTTLSClient(packet []byte) error {
+	// Get Identifier info from the Start TTLS-packet
+	ift, err := IFTDecode(packet)
+	if err != nil {
+		return err
+	}
 
-	serverIdentCh := make(chan IdentInfo)
-	clientIdentCh := make(chan IdentInfo)
+	eap, err := EAPDecode(ift.Data[4:])
+	if err != nil {
+		return err
+	}
+
+	ii := IdentInfo{
+		EAPIdentifier: eap.Identifier,
+	}
 
 	// Create two EAP-TTLS wrappers,
 	// one for the server connection and one for the client connection.
@@ -86,120 +104,146 @@ func (p *Proxy) SetupTTLS(original net.Conn, dir communicationDir) error {
 	// They both have the readIdent/writeIdent channels switched between them,
 	// so identifiers from the server are copied to the client and vice-versa
 	serverEAP := &EAPTTLSConn{
-		Conn:         p.serverConn,
-		direction:    ClientToServer,
-		readIdentCh:  serverIdentCh,
-		writeIdentCh: clientIdentCh,
+		Conn:          p.serverConn,
+		direction:     ClientToServer,
+		readIdentCh:   p.serverIdentCh,
+		writeIdentCh:  p.clientIdentCh,
+		IFTIdentifier: ift.Identifier, // Keep increasing the IFT identifiers when communicating with the server
+	}
+
+	// Add identifier info to channel, so that it can be used in the handshake process
+	p.clientIdentCh <- ii
+
+	fmt.Fprintf(outputWriter, "Setting up client->server EAP-TTLS\n")
+	c := tls.Client(serverEAP, &tls.Config{InsecureSkipVerify: true, MaxVersion: tls.VersionTLS12, DynamicRecordSizingDisabled: true})
+	err = c.Handshake()
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(outputWriter, "client->server Handshake is done!")
+	p.serverConn = c
+	return nil
+}
+
+func (p *Proxy) SetupTTLSServer(packet []byte) error {
+	// Get Identifier info from the Start TTLS-packet
+	ift, err := IFTDecode(packet)
+	if err != nil {
+		return err
+	}
+
+	eap, err := EAPDecode(ift.Data[4:])
+	if err != nil {
+		return err
+	}
+
+	ii := IdentInfo{
+		EAPIdentifier: eap.Identifier,
 	}
 
 	clientEAP := &EAPTTLSConn{
 		Conn:         p.clientConn,
 		direction:    ServerToClient,
-		readIdentCh:  clientIdentCh,
-		writeIdentCh: serverIdentCh,
+		readIdentCh:  p.clientIdentCh,
+		writeIdentCh: p.serverIdentCh,
+		hasData:      true,
+		data:         eap.Data,
 	}
 
-	c := tls.Client(serverEAP, &tls.Config{InsecureSkipVerify: true})
-	err := c.Handshake()
+	p.serverIdentCh <- ii
+
+	cfg := &tls.Config{
+		Certificates:                []tls.Certificate{tlsCert},
+		MaxVersion:                  tls.VersionTLS12,
+		DynamicRecordSizingDisabled: true,
+		ClientAuth:                  tls.RequestClientCert,
+	}
+
+	fmt.Fprintf(outputWriter, "Setting up server->client EAP-TTLS, with initial packet data set\n")
+	c := tls.Server(clientEAP, cfg)
+	err = c.Handshake()
 	if err != nil {
 		return err
 	}
-	p.serverConn = c
+	fmt.Fprintf(outputWriter, "server->client handshake done\n")
 
-	c = tls.Server(clientEAP, &tls.Config{Certificates: []tls.Certificate{tlsCert}})
 	p.clientConn = c
+
 	return nil
 }
 
-// Reader forwards all data from client to clientCh, or from server to serverCh
+// Reader forwards all data from client to server, or from server to client
 func (p *Proxy) Reader(direction communicationDir) error {
 	var src net.Conn
-	var ch chan []byte
-
-	switch direction {
-	case ClientToServer:
-		src = p.clientConn
-		ch = p.clientCh
-	case ServerToClient:
-		src = p.serverConn
-		ch = p.serverCh
-	}
-
-	defer close(ch)
+	var dest net.Conn
+	clientTTLSconfigured := false
 
 	buf := make([]byte, 32*1024)
 	for {
+		switch direction {
+		case ClientToServer:
+			src = p.clientConn
+			dest = p.serverConn
+		case ServerToClient:
+			src = p.serverConn
+			dest = p.clientConn
+		}
+
 		nr, err := src.Read(buf)
 		if nr > 0 {
-			ch <- buf[0:nr]
+			packet := buf[0:nr]
+
+			ttlsStr := ""
+			if p.useTTLS && (direction == ServerToClient || clientTTLSconfigured) {
+				ttlsStr = " [EAP-TTLS]"
+			}
+			fmt.Fprintf(outputWriter, "Packet %s%s:\n%s", direction, ttlsStr, hex.Dump(packet))
+
+			// If we've initiated a TTLS connection on the server side, we need to do the same on the client side,
+			// but we're now acting as a server, which means that the first packet we receive will be from the
+			// original connection.
+			if direction == ClientToServer && p.useTTLS && !clientTTLSconfigured {
+				clientTTLSconfigured = true
+
+				err = p.SetupTTLSServer(packet)
+				if err != nil {
+					return err
+				}
+				continue
+			}
+
+			nw, e := dest.Write(packet)
+			if e != nil {
+				return e
+			}
+			if nw < nr {
+				return io.ErrShortWrite
+			}
+
+			// If the server sent a start TTLS-packet, initiate a new TTLS-communication with the server side
+			if direction == ServerToClient && !p.useTTLS && isStartTTLS(buf) {
+				p.useTTLS = true
+				err = p.SetupTTLSClient(packet)
+				if err != nil {
+					return err
+				}
+			}
+
 		}
+
 		if err != nil {
+			if e, ok := err.(net.Error); ok {
+				if e.Timeout() {
+					src.SetReadDeadline(time.Time{})
+					fmt.Println(direction, "is timeout!")
+					continue
+				}
+			}
+
 			if err == io.EOF {
 				return nil
 			}
 			return err
-		}
-	}
-}
-
-// CopyData reads data from the client and server channels and writes them to the correct connection
-func (p *Proxy) CopyData() error {
-	var err error
-	useTTLS := false
-
-	for {
-		var buf []byte
-		var dst net.Conn
-		var direction communicationDir
-		var isStartTTLSpacket bool
-
-		select {
-		case buf = <-p.clientCh:
-			dst = p.serverConn
-			direction = ClientToServer
-		case buf = <-p.serverCh:
-			dst = p.clientConn
-			direction = ServerToClient
-
-			if isStartTTLS(buf) {
-				// If server sent a  Start-TTLS packet, we'll have to:
-				//  * Initiate handshake with the server
-				//  * Send the packet to the client, and wait for the client to initiate handshake with us
-				useTTLS = true
-				isStartTTLSpacket = true
-
-				// Note that we're now replacing our serverConnection with the wrapped version
-				p.serverConn, err = p.SetupTTLS(p.serverConn, ClientToServer)
-				if err != nil {
-					return err
-				}
-
-				// We first prepare the client connection for accepting TLS data
-				// Note that we must then send the start-packet to the client,
-				// but not through the new TTLS connection, which is why 'dst' is not updated
-				p.clientConn, err = p.SetupTTLS(p.clientConn, ServerToClient)
-				if err != nil {
-					return err
-				}
-			}
-		case <-p.done:
-			return nil
-		}
-
-		if useTTLS && !isStartTTLSpacket {
-			fmt.Fprintf(outputWriter, "Packet %s:\n", direction)
-			PrintAVP(outputWriter, buf)
-		} else {
-			_, _ = fmt.Fprintf(outputWriter, "Packet %s:\n%s", direction, hex.Dump(buf))
-		}
-
-		nb, err := dst.Write(buf)
-		if err != nil {
-			return err
-		}
-
-		if nb != len(buf) {
-			return io.ErrShortWrite
 		}
 	}
 }
@@ -219,18 +263,14 @@ func handleClient(clientConn net.Conn) error {
 	go func() {
 		err = p.Reader(ClientToServer)
 		if err != nil {
-			fmt.Fprintf(outputWriter, "%s: error from reader: %s\n", ClientToServer, err)
+			fmt.Fprintf(outputWriter, "%s: error from reader: %s (%T %v)\n", ClientToServer, err, err, err)
 		}
 	}()
 
-	go func() {
-		err = p.Reader(ServerToClient)
-		if err != nil {
-			fmt.Fprintf(outputWriter, "%s: error from reader: %s\n", ServerToClient, err)
-		}
-	}()
-
-	err = p.CopyData()
+	err = p.Reader(ServerToClient)
+	if err != nil {
+		fmt.Fprintf(outputWriter, "%s: error from reader: %s (%T %v)\n", ServerToClient, err, err, err)
+	}
 	return err
 }
 
@@ -294,6 +334,7 @@ func main() {
 			if err != nil {
 				fmt.Fprintf(outputWriter, "error from handleClient: %s\n", err)
 			}
+			log.Printf("server: client %s disconnected", conn.RemoteAddr())
 		}()
 	}
 }
